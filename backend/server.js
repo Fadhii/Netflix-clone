323991a03e854504a7035a3adcc3b2ea1f1d78a6
Fadhi <102654179+Fadhii@users.noreply.github.com>
2024-08-15 23:25:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 const corsOptions = {
-    origin:'http://localhost:3000',
+    origin:'https://netflix-clone-frontend-m98w.onrender.com/',
     credentials:true
 }
 app.use(cors(corsOptions))
