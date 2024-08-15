258138fc931ae79994ee1e0060f251eda064d609
Fadhi <102654179+Fadhii@users.noreply.github.com>
2024-08-15 22:45:30 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
 export const baseUrl = 'https://api.themoviedb.org/3'
 export const API_KEY = 'bd1a58f43445153be794a510e6198f0e'
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
-export const API_END_POINT = "http://localhost:8080/api/v1/user";
+export const API_END_POINT = "https://netflix-clone-backend-jpgp.onrender.com/api/v1/user";
